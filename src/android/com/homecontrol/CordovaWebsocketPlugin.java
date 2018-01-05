ce8c76a6edbf0da567d2faf6809b3293ff55d521
chax <mislav.cakaric@homecontrol.no>
2018-01-05 16:06:52 +0100

    Android version implemented
    * added allowAllCerts, timeout and headers properties
    * allowAllCerts - when set to true doesn't check for certificate validity
    * timeout - timeout in milliseconds
    * headers - you can send custom headers when connecting to WebSocket

--- src/android/com/homecontrol/CordovaWebsocketPlugin.java
+++ src/android/com/homecontrol/CordovaWebsocketPlugin.java
@@ -13,6 +13,7 @@ import org.json.JSONException;
 import android.util.Log;
 
 import java.util.Date;
+import java.util.Iterator;
 import java.util.UUID;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -26,6 +27,19 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.WebSocketListener;
 import okio.ByteString;
+
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 
 public class CordovaWebsocketPlugin extends CordovaPlugin {
     private static final String TAG = "CordovaWebsocketPlugin";
@@ -120,8 +134,6 @@ public class CordovaWebsocketPlugin extends CordovaPlugin {
         }
     }
 
-
-
     private class WebSocketAdvanced extends WebSocketListener {
         
         private WebSocket webSocket;
@@ -137,15 +149,50 @@ public class CordovaWebsocketPlugin extends CordovaPlugin {
                 this.callbackContext = callbackContext;
                 this.webSocketId = UUID.randomUUID().toString();
                 
-                String wsUrl = wsOptions.getString("url");
+                String wsUrl =              wsOptions.getString("url");
+                int timeout =               wsOptions.optInt("timeout", 0);
+                JSONObject wsHeaders =      wsOptions.optJSONObject("headers");
+                boolean acceptAllCerts =    wsOptions.optBoolean("acceptAllCerts", false);
                 
-                this.client = new OkHttpClient.Builder()
-                    .readTimeout(0,  TimeUnit.MILLISECONDS)
-                    .build();
-        
-                this.request = new Request.Builder()
-                    .url(wsUrl)
-                    .build();
+                
+                OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
+                Request.Builder requestBuilder = new Request.Builder();
+
+                clientBuilder.readTimeout(timeout, TimeUnit.MILLISECONDS);
+                
+                if (wsUrl.startsWith("wss://") && acceptAllCerts) {
+                    try {
+                        final X509TrustManager gullibleTrustManager = new GullibleTrustManager();
+                        final HostnameVerifier gullibleHostnameVerifier = new GullibleHostnameVerifier();
+                        final SSLContext sslContext = SSLContext.getInstance("SSL");
+                        KeyManager[] keyManagers = null;
+                        TrustManager[] trustManagers = new TrustManager[]{gullibleTrustManager};
+                        SecureRandom secureRandom = new SecureRandom();
+                        sslContext.init(keyManagers, trustManagers, secureRandom);
+                        
+                        // Create an ssl socket factory with our all-trusting manager
+                        final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+                        clientBuilder.sslSocketFactory(sslSocketFactory, gullibleTrustManager);
+                        clientBuilder.hostnameVerifier(gullibleHostnameVerifier);
+                    } catch (Exception e) {
+                        Log.e(TAG, e.getMessage());
+                    }
+                }
+
+                requestBuilder.url(wsUrl);
+
+                if (wsHeaders != null) {
+                    Iterator<String> headerNames = wsHeaders.keys();
+                    while (headerNames.hasNext()) {
+                        String headerName = headerNames.next();
+                        String headerValue = wsHeaders.getString(headerName);
+                        requestBuilder.addHeader(headerName, headerValue);
+                    }
+                }
+
+                this.client = clientBuilder.build();
+                this.request = requestBuilder.build();
 
                 final WebSocketAdvanced self = this;
 
@@ -275,6 +322,35 @@ public class CordovaWebsocketPlugin extends CordovaPlugin {
                 Log.e(TAG, e.getMessage());
             }
         }
-    
+    }
+
+    private class GullibleTrustManager implements X509TrustManager {
+        private static final String TAG = "GullibleTrustManager";
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            X509Certificate[] x509Certificates = new X509Certificate[0];
+            return x509Certificates;
+        }
+
+        @Override
+        public void checkServerTrusted(final X509Certificate[] chain,
+                                       final String authType) throws CertificateException {
+            Log.d(TAG, "authType: " + String.valueOf(authType));
+        }
+
+        @Override
+        public void checkClientTrusted(final X509Certificate[] chain,
+                                       final String authType) throws CertificateException {
+            Log.d(TAG, "authType: " + String.valueOf(authType));
+        }
+    };
+
+    private class GullibleHostnameVerifier implements HostnameVerifier {
+
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
     }
 }
